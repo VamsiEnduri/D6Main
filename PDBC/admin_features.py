96e3f55ee78f9e20e5d6c237fb4115a85c9f2cf3
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2025-12-05 12:07:21 +0530

    pdbc @ 1 day 2  @ 100000coders_sms

--- PDBC/admin_features.py
+++ PDBC/admin_features.py
@@ -11,3 +11,58 @@ def create_batch(a,b,c):
     conn.commit()
     print("batch added successfullyyy.....")
 
+def add_student(a,b,c,d,e,f):
+    q="insert into students2 (s_name,s_email,s_phnum,s_t_id,s_b_id,s_a_id) values (%s,%s,%s,%s,%s,%s)"
+    curObj.execute(q,(a,b,c,d,e,f))
+    conn.commit()
+    print("student added successfullyyy.....")
+
+def fetch_all_trainers():
+    query="select * from trainers"
+    curObj.execute(query)
+    data=curObj.fetchall()
+    print(data)
+    print("fethed all traniers data")
+
+def fetch_all_batches():
+    query="select * from batches"
+    curObj.execute(query)
+    data=curObj.fetchall()
+    print(data)
+    print("fethed all batches data")
+    
+def fetch_all_students():
+    query="select * from students2"
+    curObj.execute(query)
+    data=curObj.fetchall()
+    print(data)
+    print("fethed all students data")    
+        
+def fetch_t_b_data(id):
+    query="""
+             select t_id,t_name,t_email,b_id,b_name
+             from trainers 
+             inner join batches
+             on trainers.t_id = batches.trainer_id
+             where trainers.t_id =%s
+    """
+    curObj.execute(query,(id,))
+    data=curObj.fetchall()
+    print(data)
+    print("trainers+batch data fetched successfully........")
+
+
+def fetch_t_s_data(id):
+    query="""
+             select t_id,t_name,t_email,s_name,s_email,s_b_id
+             from trainers 
+             right join students2
+             on trainers.t_id = students2.s_t_id
+             where trainers.t_id =%s
+    """
+    curObj.execute(query,(id,))
+    data=curObj.fetchall()
+    print(data)
+    print("trainers+students data fetched successfully........")
+
+
--- PDBC/main.py
+++ PDBC/main.py
@@ -1,5 +1,5 @@
 # i wanna login as admin
-from admin_features import create_trainer,create_batch
+from admin_features import create_trainer,create_batch,add_student,fetch_all_trainers,fetch_all_batches,fetch_all_students,fetch_t_b_data,fetch_t_s_data
 from admin_login import admin_login
 admin_login_status=admin_login()
 print(admin_login_status,"5 main.py")
@@ -11,7 +11,11 @@ if admin_login_status:
            1. create trainer
            2.create batches
            3.create students
-           4.exit
+           4. fetch all trainers
+           5. fetch all batches
+           6. fetch all students
+           7. fetch trainers + their batches data only
+           8. fetch trainers + their studnets data only
         """)
         i=int(input("enetr yr choice here :--  "))
         if i== 1:
@@ -24,7 +28,29 @@ if admin_login_status:
             b_n=input("enter b_name here :--- ")
             t_id=int(input("enter t_id here :--- "))
             ad_id=int(input("enter ad_id  here :--- "))
-            create_batch(b_n,ad_id,t_id)    
+            create_batch(b_n,ad_id,t_id)  
+        elif i == 3:
+            s_n=input("enter s_name here :--- ")
+            s_e=input("enter s_email here :--- ")
+            s_ph=input("enter s_phNum  here :--- ")
+            s_t_id=int(input("enter s_trainer_id  here :--- "))
+            s_b_id=int(input("enter s_batch_id  here :--- "))
+            s_a_id=int(input("enter s_admin_id  here :--- "))
+            add_student(s_n,s_e,s_ph,s_t_id,s_b_id,s_a_id)
+        elif i == 4:
+            fetch_all_trainers()
+        elif i == 5:
+            fetch_all_batches()    
+
+        elif i ==6:
+            fetch_all_students()
+        elif i ==7 :
+            t_id=int(input("enter trainer id to get all his data with his batches data"))    
+            fetch_t_b_data(t_id)
+        elif i == 8:
+            t_id=int(input("enter trainer id to get all his data with his batches data"))    
+            fetch_t_s_data(t_id)
+
 else:
     print("not valid admin")
 
